efe9f34dee6ea42e7430ba437392972858b10867
shivv <shivpratikhande2017@gmail.com>
2024-05-31 19:25:56 +0530

    connected wallet with the backend

--- client/app/components/Wallet.tsx
+++ client/app/components/Wallet.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import Container from './Container'
+import axios from 'axios'
+
 
 function Wallet() {
+  const[bal, setBal] = useState("")
+  const[val, setVal] = useState("")
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!val) {
+        setBal("");
+        return;
+      }
+      const response = await axios.get(`http://localhost:5000/api/balance/${val}`)
+      const data = response.data
+      setBal(data.balance)
+
+    console.log(data)
+
+    
+    }
+    fetchData()
+
+
+  }, [val])
+
+  function handleChange(e:any){
+    setVal (e.target.value)
+
+  }
+  console.log(val)
+
+
+
+
+
   return (
     <div>
       <Container>
@@ -13,14 +48,15 @@ function Wallet() {
           <input
             type="text"
             placeholder='0x1'
-            className=' p-1 px-3 border-2 border-black rounded-xl ' />
+            className=' p-1 px-3 border-2 border-black rounded-xl '
+            onChange={handleChange} />
         </div>
         <div className=' mt-5'>
           <h2 className=' pb-1'> Balance</h2>
-          <input 
-          type="none"
-          className=' p-1 px-3 border-2 border-black rounded-xl bg-slate-200 ' 
-          value={"none"}/>
+          <input
+            type="none"
+            className=' p-1 px-3 border-2 border-black rounded-xl bg-slate-200 font-bold '
+            value={`${bal} `} />
         </div>
 
       </Container>
