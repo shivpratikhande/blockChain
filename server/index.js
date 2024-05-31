8cccb352d2e5fcaea63d8a421058b01ee9a9e65f
shivv <shivpratikhande2017@gmail.com>
2024-05-31 10:29:19 +0530

    done with the backeknd

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const app =  express()
+const app = express()
 const port = 5000
 const cors = require("cors")
 
@@ -18,13 +18,36 @@ app.get("/balance/:address", (req, res) => {
     const balance = balances[address] || 0;
 
     res.send({ balance });
-  });
+});
 
-app.post("/api/send",(req, res)=>{
-    
+
+function setInitialBalance(address) {
+    if (!balances[address]) {
+        balances[address] = 0
+    }
+}
+
+
+app.post("/api/send", (req, res) => {
+    const { sender, reciver, amount } = req.body
+
+    setInitialBalance(reciver) 
+    setInitialBalance(sender) 
+
+    if (balances[sender] < amount) {
+/*         alert("insufficient balance")
+ */        res.status(400).send({ balance: "not enough funds" })
+    }
+    else {
+        balances[sender] -= amount
+        balances[reciver] += amount
+        console.log(balances)
+        res.send({ balances: balances[sender] })
+    }
+
 })
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("on port 5000")
 })
